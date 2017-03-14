a8bfb1e04314e1a68037a81c3649d081fdbcc4b6
G. D. McBain <gdmcbain@protonmail.com>
2017-03-14 16:41:31 +1100

    fixed dirichlet a bit

--- finidiff/test/test_finidiff_matrix_dirichlet.py
+++ finidiff/test/test_finidiff_matrix_dirichlet.py
@@ -5,6 +5,7 @@ import unittest
 
 import numpy as np
 from scipy.misc import comb
+from scipy.sparse.linalg import spsolve
 from scipy.special import gamma
 
 from ..finidiff import finidiff_matrix_dirichlet
@@ -20,3 +21,16 @@ class TestFinidiffMatrixDirichlet(unittest.TestCase):
             self.assertTrue(
                 np.linalg.cond(
                     finidiff_matrix_dirichlet(x, q, -4, 4).toarray()) > 1e9)
+
+    def test_poisson(self, n=2**6):
+        '''Solve u" + 2 = 0 subject to u(0) = u(1) = 0
+
+        which has exact solution x * (1 - x)
+
+        '''
+
+        x = np.sort(np.random.rand(n))
+        np.testing.assert_array_almost_equal(
+            spsolve(finidiff_matrix_dirichlet(x, 2, 0, 1),
+                    -2 * np.ones_like(x)),
+            x * (1 - x))
