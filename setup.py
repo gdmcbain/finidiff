64a549f05185f244121916bbd2148c0aeaf7bd8c
G. D. McBain <gdmcbain@protonmail.com>
2017-03-14 11:50:03 +1100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='refill',
+    version='0.0.1',
+    packages=find_packages(),
+    package_data={},
+
+    install_requires=['numpy', 'scipy'],
+
+    author='G. D. McBain',
+    author_email='gmcbain@protonmail.com'
+
+)
